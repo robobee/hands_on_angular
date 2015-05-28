4d2a2ece1b6af9a91b4c19d4d1bf7cc7ae79d802
Igor Serpak <igor.serpak@gmail.com>
2015-05-28 10:39:07 -0300

    Added api querying and tests for it

--- app/scripts/services/edges.js
+++ app/scripts/services/edges.js
@@ -1,41 +1,5 @@
 'use strict';
 
-angular.module('swFrontendApp').service('edges', function() {
-  this.query = function() {
-    return [
-      {
-        name: 'Attrractive',
-        description: 'blah...',
-        category: {
-          name: 'Background'
-        },
-        requirements: [
-          { name: null, value: 'Novice', mode: 'rank' },
-          { name: 'Vigor', value: 'd6', mode: 'trait' }
-        ]
-      },
-      {
-        name: 'Attrractive',
-        description: 'blah...',
-        category: {
-          name: 'Background'
-        },
-        requirements: [
-          { name: null, value: 'Novice', mode: 'rank' },
-          { name: 'Vigor', value: 'd6', mode: 'trait' }
-        ]
-      },
-      {
-        name: 'Brawler',
-        description: 'description',
-        category: {
-          name: 'Combat'
-        },
-        requirements: [
-          { name: null, value: 'Seasoned', mode: 'rank' },
-          { name: 'Strength', value: 'd8', mode: 'trait' }
-        ]
-      }
-    ];
-  };
+angular.module('swFrontendApp').factory('edges', function($resource) {
+  return $resource('/api/edges');
 });
