1c58fc31247f83004730edf561e05e3d2fed7397
Igor Serpak <igor.serpak@gmail.com>
2015-05-28 13:20:03 -0300

    Implement simple token authentication

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < Devise::SessionsController
+
+	skip_before_action :verify_signed_out_user, only: [:destroy]
+  before_filter :authenticate_user_from_token!, except: [:create]
+
+  def create
+    user = User.find_for_database_authentication(email: params[:email])
+    if user && user.valid_password?(params[:password])
+      token = user.ensure_authentication_token
+      render json: { auth_token: token }
+    else
+      render nothing: true, status: :unauthorized
+    end
+  end
+
+  def destroy
+  	puts 'hello'
+    current_user.authentication_token = nil
+    current_user.save!
+    render json: {}
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
 
+  devise_for :users, skip: [ :sessions ]
+  as :user do
+    post '/api/login' => 'sessions#create'
+    delete '/api/logout' => 'sessions#destroy'
+  end
+
   namespace :api do
     resources :edges, only: [:index]
+    resources :users, only: [:index]
   end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
